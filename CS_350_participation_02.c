f349caf62dbd9eda5dd46ad0384ceea1009635a5
Farhan Shahbaz <49499937+Farhan12345a@users.noreply.github.com>
2020-03-02 21:48:58 -0500

    Add files via upload

---
+++ CS_350_participation_02.c
@@ -0,0 +1,33 @@
+//Franklin
+
+
+//Farhan Shahbaz
+
+
+
+#include <stdio.h>
+
+
+
+int main(){
+   
+	double num1;
+  
+	printf("Enter first number ");
+   
+	scanf("%lf", &num1);
+   
+	printf("Enter second number ");
+   
+	double num2;
+   scanf("%lf", &num2);
+   
+	double multiply = num1 * num2;
+   
+   
+	printf("Product = %.3lf", multiply);
+   
+  
+	return 0;
+
+}
